c1d37703a2bf9605fb96f52ff455b4a19ec9748f
홍성범 <sungbum132@naver.com>
2024-11-26 03:58:13 +0900

    실수로 staging 안한거 다시 커밋

--- pattern_generator.v
+++ pattern_generator.v
@@ -1,6 +1,7 @@
 module pattern_generator (
     input clk,
     input clk_time, // 랜덤 패턴 만들 경우에 필요할듯. FPGA에 구현가능한지 여부 판단후 유지/삭제 예정
+    input rst,
     input enable,
     output [3:0] pattern [15:0] //최대 16개. 난이도에 따라 GM에서 n개 추출하여 사용. 따라서 여기서 level에 맞춰 생성할 필요는 없음.
 )
